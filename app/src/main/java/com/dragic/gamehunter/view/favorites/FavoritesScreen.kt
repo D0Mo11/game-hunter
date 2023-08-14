17f5d45076df224be1f4fe66f435ac9d5b75a4c3
domagojdragic <domagoj.dragic@cobeisfresh.com>
2023-08-14 10:51:25 +0200

    feature/top_bar_scaffold - add screen top bars in scaffold

--- app/src/main/java/com/dragic/gamehunter/view/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/dragic/gamehunter/view/favorites/FavoritesScreen.kt
@@ -3,7 +3,9 @@ package com.dragic.gamehunter.view.favorites
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,35 +14,41 @@ import androidx.compose.ui.res.stringResource
 import com.dragic.gamehunter.R
 import com.dragic.gamehunter.view.uicomponents.FavoritesTopBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavoritesScreen(
     onGameClick: (Int) -> Unit,
     modifier: Modifier = Modifier,
     favoriteGamesState: List<FavoriteGameViewState>,
 ) {
-    Column(
-        modifier = modifier.fillMaxSize()
-    ) {
-        FavoritesTopBar()
-        Text(
-            modifier = Modifier
-                .padding(
-                    top = dimensionResource(id = R.dimen.favorites_text_top_padding),
-                    start = dimensionResource(id = R.dimen.favorites_text_label_top_padding),
-                    end = dimensionResource(id = R.dimen.favorites_text_label_end_padding)
-                ),
-            text = stringResource(id = R.string.favorite_games),
-            style = MaterialTheme.typography.labelLarge
-        )
-        FavoriteGames(
-            favoriteGames = favoriteGamesState,
-            onGameClick = onGameClick,
-            modifier = Modifier
-                .padding(
-                    start = dimensionResource(id = R.dimen.favorites_game_card_horizontal_padding),
-                    top = dimensionResource(id = R.dimen.favorites_game_card_top_padding),
-                    end = dimensionResource(id = R.dimen.favorites_game_card_horizontal_padding),
-                )
-        )
+    Scaffold(
+        topBar = { FavoritesTopBar() }
+    ) { paddingValues ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Text(
+                modifier = Modifier
+                    .padding(
+                        top = dimensionResource(id = R.dimen.favorites_text_top_padding),
+                        start = dimensionResource(id = R.dimen.favorites_text_label_top_padding),
+                        end = dimensionResource(id = R.dimen.favorites_text_label_end_padding)
+                    ),
+                text = stringResource(id = R.string.favorite_games),
+                style = MaterialTheme.typography.labelLarge
+            )
+            FavoriteGames(
+                favoriteGames = favoriteGamesState,
+                onGameClick = onGameClick,
+                modifier = Modifier
+                    .padding(
+                        start = dimensionResource(id = R.dimen.favorites_game_card_horizontal_padding),
+                        top = dimensionResource(id = R.dimen.favorites_game_card_top_padding),
+                        end = dimensionResource(id = R.dimen.favorites_game_card_horizontal_padding),
+                    )
+            )
+        }
     }
 }
